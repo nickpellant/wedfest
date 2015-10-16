65539cc471cb95e6bc0aae911a78e0c930690502
Nick Pellant <nick@nickpellant.com>
2015-10-16 21:20:57 +0100

    Add guests relationship to Invite serializer

--- app/serializers/invite_serializer.rb
+++ app/serializers/invite_serializer.rb
@@ -1,3 +1,5 @@
 class InviteSerializer < ActiveModel::Serializer
   attributes :id, :invite_code
+
+  has_many :guests
 end
--- spec/serializers/invite_serializer_spec.rb
+++ spec/serializers/invite_serializer_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe InviteSerializer, type: :serializer do
-  subject(:invite) { FactoryGirl.build_stubbed(:invite) }
+  subject(:invite) { FactoryGirl.create(:invite, :with_guests) }
+  let(:guests) { invite.guests }
+
+  let(:guests_relationship_data) do
+    guests.map { |guest| { id: guest.id.to_s, type: 'guests' } }
+  end
 
   let(:actual_json) { ActiveModel::SerializableResource.new(invite).to_json }
   let(:expected_json) do
@@ -11,10 +16,15 @@ RSpec.describe InviteSerializer, type: :serializer do
         type: 'invites',
         attributes: {
           invite_code: invite.invite_code
+        },
+        relationships: {
+          guests: {
+            data: guests_relationship_data
+          }
         }
       }
     }.to_json
   end
 
-  it { expect(expected_json).to eql(actual_json) }
+  it { expect(actual_json).to eql(expected_json) }
 end
