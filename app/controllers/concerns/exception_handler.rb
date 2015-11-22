12e0952416de86907f6a6fd13086f16817df4aa2
Nick Pellant <nick@nickpellant.com>
2015-11-22 16:01:15 +0000

    Update Marmite and use ExceptionRenderer

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -1,5 +1,6 @@
 module Concerns
   module ExceptionHandler
+    include Marmite::Mixins::ExceptionRenderer
     extend ActiveSupport::Concern
 
     included do
@@ -9,20 +10,6 @@ module Concerns
           status: :bad_request,
           options: { details: exception.message }
         )
-      end
-
-      # Renders a JSON error response
-      # @param code [Symbol] the error code
-      # @param status [Symbol] the HTTP status to respond with
-      # @param details [Hash] additional error details
-      def render_json_error(code:, status:, options: {})
-        error = {
-          code: I18n.t("error_codes.#{code}.code"),
-          title: I18n.t("error_codes.#{code}.message"),
-          status: status
-        }.merge(options)
-
-        render json: { errors: [error] }, status: status
       end
     end
   end
