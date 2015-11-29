87590fe971601cd58893c223dec3dc9d79b4136a
Nick Pellant <nick@nickpellant.com>
2015-11-29 18:04:02 +0000

    Add accommodation factory options

--- spec/factories/accommodations.rb
+++ spec/factories/accommodations.rb
@@ -1,8 +1,36 @@
 FactoryGirl.define do
   factory :accommodation do
-    initial_availability 10
     name { Faker::Commerce.product_name }
-    price_pence 100
-    sleeps 2
+
+    trait :budget_bell_tent do
+      initial_availability 10
+      name 'Budget Bell Tent'
+      price 106.00
+      sleeps 2
+    end
+
+    trait :luxury_bell_tent do
+      name 'Luxury Bell Tent'
+      price 160.00
+      sleeps 2
+    end
+
+    trait :group_bell_tent do
+      name 'Group Bell Tent'
+      price 160.00
+      sleeps 4
+    end
+
+    trait :tent_share do
+      name 'Tent Share'
+      price 33.50
+      sleeps 1
+    end
+
+    trait :byo do
+      name 'Bring Your Own'
+      price 0.00
+      sleeps 1
+    end
   end
 end
